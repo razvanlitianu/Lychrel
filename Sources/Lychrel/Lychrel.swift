8ba71330e0a5d3791728c56344222009e2983156
Razvan Litianu <litianu.razvan@gmail.com>
2022-07-12 10:24:17 +0300

    Create Lychrel

--- Sources/Lychrel/Lychrel.swift
+++ Sources/Lychrel/Lychrel.swift
@@ -1,6 +1,31 @@
-public struct Lychrel {
-    public private(set) var text = "Hello, World!"
+import BigInt
 
-    public init() {
+public struct Lychrel {
+    fileprivate static func converge(_ n: BigInt, _ iteration: Int, limit: Int) -> Int {
+        if !isPalindrome(n), iteration < limit {
+            return converge(n + reversed(n), iteration + 1, limit: limit)
+        } else {
+            return iteration
+        }
+    }
+    
+    public static func convergesAtIteration(n: BigInt, limit: Int) -> Int {
+        return converge(n, 0, limit: limit)
+    }
+    
+    static func isPalindrome(_ n: BigInt) -> Bool {
+        let string = String(n)
+        
+        for (offset, element) in string.enumerated() {
+            let i = string.index(string.endIndex, offsetBy: -offset - 1)
+            if element != string[i] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    static func reversed(_ n: BigInt) -> BigInt {
+        BigInt(String(String(n).reversed()))!
     }
 }
--- Tests/LychrelTests/LychrelTests.swift
+++ Tests/LychrelTests/LychrelTests.swift
@@ -1,11 +1,59 @@
 import XCTest
+import BigInt
 @testable import Lychrel
 
 final class LychrelTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Lychrel().text, "Hello, World!")
+    
+    let limit = 1_000
+    
+    func testLychrel() {
+        convergesAtIteration(n: 1, interation: 0)
+        convergesAtIteration(n: 2, interation: 0)
+        convergesAtIteration(n: 10, interation: 1)
+        convergesAtIteration(n: 11, interation: 0)
+        convergesAtIteration(n: 19, interation: 2)
+        convergesAtIteration(n: 78, interation: 4)
+        convergesAtIteration(n: 89, interation: 24)
+        
+        doesNotConverge(n: 196)
+    }
+    
+    func doesNotConverge(n: BigInt, file: StaticString = #file, line: UInt = #line) {
+        convergesAtIteration(n: n, interation: limit)
+    }
+    
+    func convergesAtIteration(n: BigInt, interation: Int, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(interation, Lychrel.convergesAtIteration(n: n, limit: limit), file: file, line: line)
+    }
+    
+    func testPalindromes() {
+        isPalindrome(1)
+        isPalindrome(11)
+        isPalindrome(121)
+        isPalindrome(1234321)
+    }
+    
+    func testNonPalindromes() {
+        isNotPalindrome(10)
+        isNotPalindrome(12331)
+        isPalindrome(12344321)
+    }
+    
+    func testIsReversed() {
+        isReversed(12, reversed: 21)
+        isReversed(123, reversed: 321)
+        isReversed(1221, reversed: 1221)
+    }
+    
+    func isPalindrome(_ n: BigInt, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertTrue(Lychrel.isPalindrome(n), file: file, line: line)
+    }
+    
+    func isNotPalindrome(_ n: BigInt, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertFalse(Lychrel.isPalindrome(n), file: file, line: line)
+    }
+    
+    func isReversed(_ n: BigInt, reversed: BigInt, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(reversed, Lychrel.reversed(n), file: file, line: line)
     }
 }
